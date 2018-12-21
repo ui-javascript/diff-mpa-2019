9853663e8c2dc5d2c5e0b7f87a604933c8bb1ace
luo0412 <1095847440@qq.com>
2018-12-21 13:59:21 +0800

    adjust

---
+++ webpack4-react16-bulma-leinov-mpa/_study/react16/portal.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.scss";
+
+console.log('hello portal')
+
+ReactDOM.render(<App />, document.getElementById("root"));

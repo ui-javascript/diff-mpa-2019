ac2bf662aab381d16b903102b8a47c0a6c29a02e
luo0412 <1095847440@qq.com>
2019-06-03 10:41:27 +0800

    [结构]small change

--- webpack4-react16-mobx-bulma-leinov-config-mpa/config/index.js
+++ webpack4-react16-mobx-bulma-leinov-config-mpa/config/index.js
@@ -2,8 +2,9 @@ module.exports = {
 
     // 注意{}之间不要有空格!!
     // entries: './_pages/{index,todo}/*.js',
-    entries: './_pages/index/table.js',
+    // entries: './_pages/index/table.js',
     // entries: './_pages/todo/*.js',
     // entries: './_study/react16/*.js',
+    entries: './_study/hooks/*.js',
     // entries: './_study/{cmpt,react16}/*.js',
 }
